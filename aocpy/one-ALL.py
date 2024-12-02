bd0ee873fdd522fc8137bb9d39b5c99e7dace394
Kroko-fant <schrottsuperdeluxe@gmail.com>
2024-12-02 01:07:59 +0100

    Initial commit

---
+++ aocpy/one-ALL.py
@@ -0,0 +1,15 @@
+with open("../data/one-one.txt", "r") as f:
+    data = f.readlines()
+
+one = []
+two = []
+for line in data:
+    split = line.replace('\n', '').split('   ')
+    one.append(int(split[0]))
+    two.append(int(split[1]))
+
+one.sort()
+two.sort()
+
+print(sum([abs(one[x] - two[x]) for x in range(len(one))]))
+
